96ab47d0aa56ac2abf24dd73cc1c19cbcdc94604
Brett Botto <brettbotto@gmail.com>
2021-04-29 09:31:22 -0400

    remove winston to use console.log...adding logs for debugging aws

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,6 +3,7 @@ import { getMedications } from './routes/medication';
 
 export const buildRouter = (): Router => {
   const router = Router();
+  console.log('api.6');
   router.use('/medication', getMedicationRouter());
   // router.use('/patient', getMedicationRouter());
   return router;
@@ -10,6 +11,7 @@ export const buildRouter = (): Router => {
 
 export const getMedicationRouter = (): Router => {
   const router = Router();
+  console.log('api.14');
   router.get('/', getMedications);
   return router;
 };
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import { app } from './app';
 import { isLocal } from './config';
 
 export const main = (): void => {
+  console.log('main.6');
   serverlessExpress({ app });
+  console.log('main.8');
 };
 
 export const localServer = (): void => {
   const port = process.env.PORT ?? 4000;
   app.listen(port, () => console.log(`Listening on port: ${port}`));
 };
-
-console.log(process.env);
 
 if (isLocal()) localServer();
